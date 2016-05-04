81e291c80f6e144f30af893e850170de85ae7b7d
Shane Rosse <rosse@usc.edu>
2016-05-03 18:56:15 -0700

    Pushing for Robert

---
+++ Demo/Classes/Common/Demo-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Demo-Bridging-Header.h
+//  Demo
+//
+//  Created by Shane Rosse on 3/1/16.
+//  Copyright © 2016 Shane Rosse. All rights reserved.
+//
+
+#ifndef Demo_Bridging_Header_h
+#define Demo_Bridging_Header_h
+
+
+#endif /* Demo_Bridging_Header_h */
